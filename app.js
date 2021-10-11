eb13e34dfd268d7009ff52b30e793b9c48094676
Kittiphon <61160086@go.buu.ac.th>
2021-10-11 15:58:38 +0700

    TestappV2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const port = process.env.port || 8080
 const app = express()
 
-app.get('/user', (req, res) => {
+app.get('/', (req, res) => {
     res.send('61160086')
 })
 
